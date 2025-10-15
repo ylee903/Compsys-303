94c6f0c824762fd5ab4a38480d1e0a8e93f0d1a5
Samuel Lee <ylee903@UoA.auckland.ac.nz>
2025-10-15 16:40:15 +1300

    refactoring base

---
+++ project/DE2-115/software/pacemakerniosproject/obj/default/main.d
@@ -0,0 +1,55 @@
+obj/default/main.o: main.c ../pacemakerniosproject_bsp/system.h \
+ ../pacemakerniosproject_bsp/linker.h Pacemaker.h timing.h \
+ ../pacemakerniosproject_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../pacemakerniosproject_bsp//HAL/inc/io.h \
+ ../pacemakerniosproject_bsp//HAL/inc/alt_types.h \
+ ../pacemakerniosproject_bsp//HAL/inc/alt_types.h \
+ ../pacemakerniosproject_bsp//HAL/inc/sys/alt_irq.h \
+ ../pacemakerniosproject_bsp//HAL/inc/nios2.h \
+ ../pacemakerniosproject_bsp//HAL/inc/sys/alt_timestamp.h \
+ ../pacemakerniosproject_bsp//drivers/inc/altera_avalon_timer.h \
+ ../pacemakerniosproject_bsp//HAL/inc/sys/alt_dev.h \
+ ../pacemakerniosproject_bsp//HAL/inc/sys/alt_llist.h \
+ ../pacemakerniosproject_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../pacemakerniosproject_bsp//HAL/inc/sys/alt_warning.h \
+ ../pacemakerniosproject_bsp//HAL/inc/sys/alt_alarm.h \
+ ../pacemakerniosproject_bsp//HAL/inc/sys/alt_llist.h \
+ ../pacemakerniosproject_bsp//HAL/inc/priv/alt_alarm.h
+
+../pacemakerniosproject_bsp/system.h:
+
+../pacemakerniosproject_bsp/linker.h:
+
+Pacemaker.h:
+
+timing.h:
+
+../pacemakerniosproject_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../pacemakerniosproject_bsp//HAL/inc/io.h:
+
+../pacemakerniosproject_bsp//HAL/inc/alt_types.h:
+
+../pacemakerniosproject_bsp//HAL/inc/alt_types.h:
+
+../pacemakerniosproject_bsp//HAL/inc/sys/alt_irq.h:
+
+../pacemakerniosproject_bsp//HAL/inc/nios2.h:
+
+../pacemakerniosproject_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../pacemakerniosproject_bsp//drivers/inc/altera_avalon_timer.h:
+
+../pacemakerniosproject_bsp//HAL/inc/sys/alt_dev.h:
+
+../pacemakerniosproject_bsp//HAL/inc/sys/alt_llist.h:
+
+../pacemakerniosproject_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../pacemakerniosproject_bsp//HAL/inc/sys/alt_warning.h:
+
+../pacemakerniosproject_bsp//HAL/inc/sys/alt_alarm.h:
+
+../pacemakerniosproject_bsp//HAL/inc/sys/alt_llist.h:
+
+../pacemakerniosproject_bsp//HAL/inc/priv/alt_alarm.h:
